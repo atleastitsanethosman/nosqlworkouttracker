906da7a496f208a1b0db0f1c20d4de7cc0781760
atleastitsanethosman <79068217+atleastitsanethosman@users.noreply.github.com>
2021-08-09 18:23:31 -0400

    router files and work on model

--- server.js
+++ server.js
@@ -1,14 +1,17 @@
+const path = require('path')
 const express = require("express");
 const mongoose = require("mongoose");
+const routes = require("./routes")
 
 const PORT = process.env.PORT || 3000;
 
 const app = express();
 
-app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static(path.join(__dirname, 'public')));
 
-app.use(express.static("public"));
+app.use(routes);
 
 mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workout", {
   useNewUrlParser: true,
